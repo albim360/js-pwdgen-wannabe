e9e3eb76cd02691dfc71d98a5c67d209d70be01d
Alberto <74522184+albim360@users.noreply.github.com>
2023-01-30 16:43:29 +0100

    FEAT fine esercizio base senza personalizzazioni

--- js/app.js
+++ js/app.js
@@ -1,2 +1,19 @@
 console.log('Imposto elemento HTML con id outputPassword');
 const outputPassword = document.getElementById('outputPassword');
+
+let nome 
+let cognome
+let colore
+let numero
+
+
+nome = prompt('Hey, come ti chiami?')
+cognome = prompt('Wow, ed il tuo cognome?')
+colore = prompt('Bene, e qual è il tuo colore preferito?')
+numero = prompt('Bel colore, e qual è il tuo numero preferito?')
+
+console.log(nome + cognome + colore + numero)
+
+
+
+outputPassword.innerHTML = nome + cognome + colore + numero;
